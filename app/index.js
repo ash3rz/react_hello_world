eee3ed2438522911d36a1c0533df5a4e78a85df7
Ashley Ramsey <aramsey@iplantcollaborative.org>
2017-02-05 16:33:57 -0700

    Hello World

---
+++ app/index.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Hello = React.createClass({
+    render: function () {
+        return (
+            <div>Hello World!</div>
+        )
+    }
+});
+
+ReactDOM.render(
+    <Hello />,
+    document.getElementById('app')
+);
