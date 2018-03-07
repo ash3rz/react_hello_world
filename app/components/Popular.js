f56c4cd8d75b513ffbcf3bb44c558a1ee5eeeafd
Ashley Ramsey <aramsey@iplantcollaborative.org>
2018-03-07 16:42:22 -0700

    Add state to manage in Popular

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,21 +1,46 @@
 var React = require('react');
 
 class Popular extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedLanguage: 'All'
+        };
+
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+
+    updateLanguage(lang) {
+        this.setState(function() {
+            return {
+                selectedLanguage: lang
+            }
+        });
+    }
+
     render() {
         var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-
+        console.log('Render function!', this);
         return (
             <ul className='languages'>
-                {languages.map(function (lang){
+                {languages.map(function (lang) {
+                    console.log("I'm in the map function, new context!", this);
                     return (
-                        <li key={lang}>
+                        <li
+                            style={lang === this.state.selectedLanguage ? { color: '#d0021b' } : null}
+                            onClick={this.updateLanguage.bind(null, lang)}
+                            key={lang}>
                             {lang}
                         </li>
                     )
-                })}
+                }, this)}
             </ul>
         )
     }
+    // NOTE: We're passing in 'this' as a secondary parameter to the map function.
+    // This moves the outer context into the inner function
 }
 
 module.exports = Popular;
