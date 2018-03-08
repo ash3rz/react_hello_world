796a0804f62ae106e018a4d5477e697022ed98fc
Ashley Ramsey <aramsey@iplantcollaborative.org>
2018-03-08 16:35:15 -0700

    Refactor Popular to have a stateless functional component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,30 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+function SelectLanguage(props) {
+    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+    return (
+        <ul className='languages'>
+            {languages.map((lang) => {
+                console.log("I'm in the map function, new context!", this);
+                return (
+                    <li
+                        style={lang === props.selectedLanguage ? { color: '#d0021b' } : null}
+                        onClick={props.onSelect.bind(null, lang)}
+                        key={lang}>
+                        {lang}
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+SelectLanguage.propTypes = {
+    selectedLanguage: PropTypes.string.isRequired,
+    onSelect: PropTypes.func.isRequired
+};
 
 class Popular extends React.Component {
 
@@ -21,22 +47,13 @@ class Popular extends React.Component {
     }
 
     render() {
-        var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
         console.log('Render function!', this);
         return (
-            <ul className='languages'>
-                {languages.map((lang) => {
-                    console.log("I'm in the map function, new context!", this);
-                    return (
-                        <li
-                            style={lang === this.state.selectedLanguage ? { color: '#d0021b' } : null}
-                            onClick={this.updateLanguage.bind(null, lang)}
-                            key={lang}>
-                            {lang}
-                        </li>
-                    )
-                })}
-            </ul>
+            <div>
+                <SelectLanguage
+                    selectedLanguage={this.state.selectedLanguage}
+                    onSelect={this.updateLanguage}/>
+            </div>
         )
     }
     // The arrow function ALWAYS makes it so that the outer context
