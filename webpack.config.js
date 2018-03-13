acca5d95b4ce117211271a5d3e5a1c813f70c816
Ashley Ramsey <aramsey@iplantcollaborative.org>
2018-03-13 15:55:21 -0700

    Fix bug on browser refresh saying "Cannot get /path"

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,17 @@ module.exports = {
     entry: './app/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js'
+        filename: 'index_bundle.js',
+        publicPath: '/' // publicPath will set the base path for all of our assets
     },
     module: {
         rules: [
             {test: /\.(js)$/, use: 'babel-loader'},
             {test: /\.css$/, use: ['style-loader', 'css-loader']}
         ]
+    },
+    devServer: {
+        historyApiFallback: true //whenever our app sees a url /*, it will redirect back to /, then reactRouter will load the route initially requested
     },
     plugins: [
         new HtmlWebpackPlugin({
